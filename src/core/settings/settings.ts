ae5b00f42eec1501c7acf0d44a574657dacfe508
max-pc <cep211993@gmail.com>
2025-09-02 17:05:10 +0300

    Ðedit mongoDb link

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -1,6 +1,7 @@
 
 export const SETTINGS = {
     PORT: process.env.PORT || 5003,
-    MONGO_URL: process.env.MONGO_URL || "mongodb+srv://cep211993:vdkf3kot3@cluster0.j4naknt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    MONGO_URL: process.env.MONGO_URL || "mongodb+srv://cep211993:cepencomax@cluster0.j4naknt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     DB_NAME: process.env.DB_NAME || 'blogger-platform',
 }
+
