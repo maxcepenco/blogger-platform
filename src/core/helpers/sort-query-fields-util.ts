fbbb32ec31dd1dfd1060aa3aea980b80faa678fc
massimo555 <cep211993@gmail.com>
2025-09-13 18:17:09 +0300

    add sortDirection default desc

--- src/core/helpers/sort-query-fields-util.ts
+++ src/core/helpers/sort-query-fields-util.ts
@@ -5,7 +5,7 @@ import {SortQueryFilterType} from "../types/sortQueryFilter.type";
 export const sortQueryFieldsUtil = (query: SortQueryFieldsType): SortQueryFilterType => {
     const pageNumber = !isNaN(Number(query.pageNumber)) ? Number(query.pageNumber) : 1
     const pageSize =   !isNaN(Number(query.pageSize)) ? Number(query.pageSize) : 10
-    const sortDirection: "asc" | "desc" = query.sortDirection === "desc" ? "desc" : "asc";
+    const sortDirection: "asc" | "desc" = query.sortDirection === "asc" ? "asc" : "desc"; // ✅ По умолчанию "desc"
     const sortBy = query.sortBy ?  query.sortBy: 'createdAt'
 
     return  {
