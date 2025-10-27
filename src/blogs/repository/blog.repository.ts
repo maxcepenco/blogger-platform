0b6cf84f665da9f19ab6ecb96d7cbed5c708a52b
max-pc <cep211993@gmail.com>
2025-10-27 13:16:49 +0300

    add mongoose blog.repository.ts, blog.servece.ts

--- src/blogs/repository/blog.repository.ts
+++ src/blogs/repository/blog.repository.ts
@@ -6,6 +6,15 @@ import {BlogModel} from "./blog.model";
 @injectable()
 export class BlogRepository {
 
+    async saveCreatedBlog(blog: BlogDocument) {
+       const saved = await blog.save()
+        return saved._id.toString();
+    }
+
+    async saveUpdatedBlog(user: BlogDocument) {
+        await user.save()
+        return true
+    }
 
     async findById(id: string): Promise<BlogDocument | null> {
 
@@ -16,48 +25,46 @@ export class BlogRepository {
         return result
     }
 
-    async createBlog(newBlog: Blog): Promise<string> {
-        // const createdBlog = await BlogModel.create(newBlog);
-        // return createdBlog._id.toString()
+    // async createBlog(newBlog: Blog): Promise<string> {
+    //     // const createdBlog = await BlogModel.create(newBlog);
+    //     // return createdBlog._id.toString()
+    //
+    //     const blogInstance = new BlogModel(newBlog)
+    //     blogInstance.name = newBlog.name
+    //     blogInstance.description = newBlog.description
+    //     blogInstance.createdAt = newBlog.createdAt
+    //     blogInstance.isMembership = newBlog.isMembership
+    //     await blogInstance.save()
+    //
+    //     return blogInstance._id.toString()
+    // }
 
-        const blogInstance = new BlogModel(newBlog)
-        blogInstance.name = newBlog.name
-        blogInstance.description = newBlog.description
-        blogInstance.createdAt = newBlog.createdAt
-        blogInstance.isMembership = newBlog.isMembership
-        await blogInstance.save()
+    // async updateBlog(id: string, blog: BlogInputModel): Promise<boolean> {
+    //     // const updatedResult = await BlogModel.updateOne(
+    //     //     {_id: id},
+    //     //     {
+    //     //         $set: {
+    //     //             name: blog.name,
+    //     //             description: blog.description,
+    //     //             websiteUrl: blog.websiteUrl
+    //     //         }
+    //     //     }
+    //     // )
+    //     //
+    //     // return updatedResult.matchedCount === 1
 
-        return blogInstance._id.toString()
-    }
-
-    async updateBlog(id: string, blog: BlogInputModel): Promise<boolean> {
-        // const updatedResult = await BlogModel.updateOne(
-        //     {_id: id},
-        //     {
-        //         $set: {
-        //             name: blog.name,
-        //             description: blog.description,
-        //             websiteUrl: blog.websiteUrl
-        //         }
-        //     }
-        // )
-        //
-        // return updatedResult.matchedCount === 1
-
-        const blogInstance = await this.findById(id)
-        if (!blogInstance) return false
-
-        blogInstance.name = blog.name
-        blogInstance.description = blog.description
-        blogInstance.websiteUrl = blog.websiteUrl
-        await blogInstance.save()
-
-        return true
-    }
+    //     const blogInstance = await this.findById(id)
+    //     if (!blogInstance) return false
+    //
+    //     blogInstance.name = blog.name
+    //     blogInstance.description = blog.description
+    //     blogInstance.websiteUrl = blog.websiteUrl
+    //     await blogInstance.save()
+    //
+    //     return true
+    // }
 
     async deleteBlog(id: string): Promise<boolean> {
-        // const deleteResult = await BlogModel.deleteOne({_id: id});
-        // return deleteResult.deletedCount === 1
 
         const blogInstance = await this.findById(id)
         if (!blogInstance) return false
